a313bb217596157fd5355ad7099074eb466fe0f6
Madan <madanmohan7272906844@gmail.com>
2024-07-20 00:55:08 +0530

    nav

--- client/src/components/EndTicket.tsx
+++ client/src/components/EndTicket.tsx
@@ -1,8 +1,40 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 function EndTicket() {
   return (
-    <div>EndTicket</div>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>End a Ticket</CardTitle>
+          {/* <CardDescription>Manual/QR</CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Ticket ID</Label>
+                <Input id="name" placeholder="Ticket id number" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>End</Button>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
 
-export default EndTicket
+export default EndTicket;
--- client/src/components/StartTicket.tsx
+++ client/src/components/StartTicket.tsx
@@ -1,7 +1,53 @@
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 function StartTicket() {
+  const [TicketId, setTicketId] = useState('');
+  const [VehicleNum, setVehicleNum] = useState('')
+
+  const ToStartTicket = async() => {
+    // const response = 
+  }
+
+
   return (
-    <div>StartTicket</div>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Start a Ticket</CardTitle>
+          {/* <CardDescription>Manual/QR</CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="idNum">Ticket ID</Label>
+                <Input onChange={(e) => setTicketId(e.target.value)} value={TicketId} id="idNum" placeholder="Ticket id number" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="Vnumber">Vehicle Number</Label>
+                <Input onChange={(e) => setVehicleNum(e.target.value)} value={VehicleNum} id="Vnumber" placeholder="Vehicle number" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Start</Button>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
 
-export default StartTicket
+export default StartTicket;
